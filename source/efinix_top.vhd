776744fc3465f742512337c3e45c86664e367ddd
Jari Honkanen <jaarhonka@yahoo.com>
2023-04-10 14:15:03 +0300

    clean up not yet implemented top pins

--- source/efinix_top.vhd
+++ source/efinix_top.vhd
@@ -9,13 +9,6 @@ entity efinix_top is
         clock   : std_logic;
         uart_rx : in std_logic;
         uart_tx : out std_logic
-
-        -- tft_spi_simo  : in std_logic
-        -- tft_spi_mosi  : out std_logic
-        -- tft_spi_cs    : out std_logic
-        -- tft_spi_clock : out std_logic
-        -- tft_reset     : out std_logic
-        -- tft_1_when_register_and_0_when_command : out std_logic
     );
 end entity efinix_top;
 

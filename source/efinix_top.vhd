60226e78e797347d86d879ffc217e02c0c3b1932
Jari Honkanen <jaarhonka@yahoo.com>
2023-04-13 16:37:43 +0300

    start debugging ram write port

--- source/efinix_top.vhd
+++ source/efinix_top.vhd
@@ -1,11 +1,102 @@
 library ieee;
     use ieee.std_logic_1164.all;
     use ieee.numeric_std.all;
+------------------------------------------------------------------------
+package ram_write_pkg is
+
+    type ram_write_port_record is record
+        write_enabled_with_1    : std_logic;
+        write_buffer            : std_logic_vector(15 downto 0);
+        write_address           : integer;
+        write_is_ready_pipeline : std_logic_vector(1 downto 0);
+    end record;
+
+    constant init_ram_write_port : ram_write_port_record := ('0', (others => '0'), 0, (others => '0'));
+
+------------------------------------------------------------------------
+    procedure create_ram_write_port (
+        signal self : inout ram_write_port_record);
+------------------------------------------------------------------------
+
+    procedure write_ram (
+        signal self : inout ram_write_port_record;
+        data_in     : in std_logic_vector(15 downto 0);
+        address_in  : in integer);
+
+------------------------------------------------------------------------
+    procedure write_data_to_ram (
+        data_in                     : in std_logic_vector(15 downto 0);
+        address_in                  : in integer;
+        signal write_enabled_with_1 : inout std_logic;
+        signal write_buffer         : inout std_logic_vector(15 downto 0);
+        signal write_address        : inout integer);
+
+------------------------------------------------------------------------
+    function ram_write_is_ready ( self : ram_write_port_record)
+        return boolean;
+
+end package ram_write_pkg;
+
+package body ram_write_pkg is
+------------------------------------------------------------------------
+    procedure create_ram_write_port
+    (
+        signal self : inout ram_write_port_record
+    ) is
+    begin
+        self.write_is_ready_pipeline <= self.write_is_ready_pipeline(0) & '0';
+        self.write_enabled_with_1 <= '0';
+    end create_ram_write_port;
+------------------------------------------------------------------------
+    procedure write_ram
+    (
+        signal self : inout ram_write_port_record;
+        data_in     : in std_logic_vector(15 downto 0);
+        address_in  : in integer
+    ) is
+    begin
+        self.write_enabled_with_1       <= '1';
+        self.write_buffer               <= data_in;
+        self.write_address              <= address_in;
+        self.write_is_ready_pipeline(0) <= '1';
+    end write_ram;
+------------------------------------------------------------------------
+    function ram_write_is_ready
+    (
+        self : ram_write_port_record
+    )
+    return boolean
+    is
+    begin
+        return self.write_is_ready_pipeline(self.write_is_ready_pipeline'left) = '1';
+    end ram_write_is_ready;
+------------------------------------------------------------------------
+    procedure write_data_to_ram
+    (
+        data_in                     : in std_logic_vector(15 downto 0);
+        address_in                  : in integer;
+        signal write_enabled_with_1 : inout std_logic;
+        signal write_buffer         : inout std_logic_vector(15 downto 0);
+        signal write_address        : inout integer
+    ) is
+    begin
+        write_enabled_with_1 <= '1';
+        write_buffer         <= data_in;
+        write_address        <= address_in;
+    end write_data_to_ram;
+------------------------------------------------------------------------
+end package body ram_write_pkg;
+
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+library ieee;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
 
     use work.ram_configuration_pkg.all;
     use work.fpga_interconnect_pkg.all;
     use work.ram_read_port_pkg.all;
-    use work.ram_write_port_pkg.all;
+    use work.ram_write_pkg.all;
 
 entity efinix_top is
     port (
@@ -23,7 +114,6 @@ architecture rtl of efinix_top is
 
     signal ram               : integer_array(0 to lookup_table_size - 1);
     signal ram_read_port     : ram_read_port_record;
-    signal ram_write_port    : ram_write_port_record;
     signal read_counter : natural range 0 to lookup_table_size - 1 := 0;
 
     signal test_ram_write_counter : natural range 0 to 250 := 0;
@@ -40,6 +130,8 @@ architecture rtl of efinix_top is
     signal write_enabled_with_1 : std_logic := '0';
     signal write_address : integer range 0 to 1023;
     signal write_buffer : std_logic_vector(15 downto 0);
+
+    signal ram_write_port : ram_write_port_record := init_ram_write_port;
 
 
 begin
@@ -67,36 +159,8 @@ begin
 
     process(clock_120Mhz)
     ------------------------------------------------------------------------
-        procedure write_data_to_ram
-        (
-            do_it : boolean
-        ) is
-        begin
-            write_enabled_with_1 <= '1';
-            write_buffer <= std_logic_vector(to_unsigned(get_data(bus_from_communications),16));
-            if write_address < 1023 then
-                write_address <= write_address + 1;
-            else
-                write_address <= 0;
-            end if;
-            
-        end write_data_to_ram;
-    ------------------------------------------------------------------------
-        procedure make_ram_write
-        (
-            do_it : boolean
-        ) is
-        begin
-            write_enabled_with_1 <= '0';
-        end make_ram_write;
-    ------------------------------------------------------------------------
     begin
         if rising_edge(clock_120Mhz) then
-
-            make_ram_write(true);
-            if write_enabled_with_1 = '1' then
-                test_ram(write_address) <= write_buffer;
-            end if;
 
             read_enabled_with_1 <= '0';
             if data_is_requested_from_address(bus_from_communications, 10000) then
@@ -114,11 +178,36 @@ begin
             else
                 ram_read_is_ready <= false;
             end if;
-
-            if write_from_bus_is_requested(bus_from_communications) then
-                write_data_to_ram(true);
+        ------------------------------------------------------------------------
+            -- create_ram_write_port(ram_write_port);
+            ram_write_port.write_is_ready_pipeline <= ram_write_port.write_is_ready_pipeline(0) & '0';
+            ram_write_port.write_enabled_with_1    <= '0';
+            if ram_write_port.write_enabled_with_1 = '1' then
+                test_ram(write_address) <= write_buffer;
             end if;
 
+            if write_from_bus_is_requested(bus_from_communications) then
+                write_data_to_ram(
+                    data_in              => std_logic_vector(to_unsigned(get_data(bus_from_communications),16)),
+                    address_in           => write_address,
+                    write_enabled_with_1 => ram_write_port.write_enabled_with_1,
+                    write_buffer         => write_buffer,
+                    write_address        => write_address);
+
+                ram_write_port.write_is_ready_pipeline(0) <= '1';
+
+
+
+            end if;
+
+            if ram_write_is_ready(ram_write_port) then
+                if write_address < 1023 then
+                    write_address <= write_address + 1;
+                else
+                    write_address <= 0;
+                end if;
+            end if;
+        ------------------------------------------------------------------------
 
         end if;
     end process;
